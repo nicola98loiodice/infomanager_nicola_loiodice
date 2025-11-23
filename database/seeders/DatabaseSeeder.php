3e7dfeaaee22c747e4177b6fba11c39e376658b1
Nicola Loiodice <loiodicenicola@libero.it>
2025-11-23 20:08:34 +0100

    Sistemata chiamata ai seeder tramite databaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,10 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        $this->call(
+        $this->call([
             AdminSeeder::class,
             OperatorSeeder::class,
+        ]
         );
     }
 }
